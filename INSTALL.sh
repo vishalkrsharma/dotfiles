3200beb1cb01f57d94fa59dcb2edc198bc5d762c
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-02-09 21:03:04 +0530

    feat: add gitconfig

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,4 @@
 # POLYBAR
 ln -nfs ~/dotfiles/.config/polybar/config.ini ~/.config/polybar/config.ini
 ln -nfs ~/dotfiles/.config/polybar/launch.sh ~/.config/polybar/launch.sh
+ln -nfs ~/dotfiles/.gitconfig ~/.gitconfig
