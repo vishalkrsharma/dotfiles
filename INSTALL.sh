066e746d94de1c751b2f93d9dc1f94954dc51ce0
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-02-09 21:44:03 +0530

    feat: add i3 alacritty

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,13 @@
-# POLYBAR
+# polybar
 ln -nfs ~/dotfiles/.config/polybar/config.ini ~/.config/polybar/config.ini
 ln -nfs ~/dotfiles/.config/polybar/launch.sh ~/.config/polybar/launch.sh
+
+# gitconfig
 ln -nfs ~/dotfiles/.gitconfig ~/.gitconfig
+
+# i3wm
+ln -nfs ~/dotfiles/.config/i3/config ~/.config/i3/config
+
+# alacritty
+ln -nfs ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -nfs ~/dotfiles/.config/alacritty/dracula.toml ~/.config/alacritty/dracula.toml
