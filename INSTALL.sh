b469d04f518e5709fc252055765dfe0fb2b1c2a4
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-02-10 01:59:32 +0530

    feat: add SETUP zsh rofi

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,5 @@
 # polybar
+mkdir .config/polybar
 ln -nfs ~/dotfiles/.config/polybar/config.ini ~/.config/polybar/config.ini
 ln -nfs ~/dotfiles/.config/polybar/launch.sh ~/.config/polybar/launch.sh
 
@@ -6,8 +7,18 @@ ln -nfs ~/dotfiles/.config/polybar/launch.sh ~/.config/polybar/launch.sh
 ln -nfs ~/dotfiles/.gitconfig ~/.gitconfig
 
 # i3wm
+mkdir .config/i3
 ln -nfs ~/dotfiles/.config/i3/config ~/.config/i3/config
 
 # alacritty
+mkdir .config/alacritty
 ln -nfs ~/dotfiles/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -nfs ~/dotfiles/.config/alacritty/dracula.toml ~/.config/alacritty/dracula.toml
+
+# rofi
+mkdir .config/rofi
+ln -nfs ~/dotfiles/.config/rofi/config.rasi ~/.config/rofi/config.rasi
+
+# zsh
+ln -nfs ~/dotfiles/.zshrc ~/.zshrc
+
---
+++ SETUP.sh
@@ -0,0 +1,8 @@
+# refresh packages list
+sudo pacman -Syyu
+
+# install packages
+sudo pacman -S firefox vim vlc rofi polybar alacritty lf
+
+# install fonts
+sudo pacman -S ttf-jetbrains-mono-nerd ttf-cascadia-codei
